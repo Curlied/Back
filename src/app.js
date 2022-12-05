485e874fb9c52c50ba99a3005c7ac2e492d23b97
ferrere-fabrice <ferrerefabrice@outlook.fr>
2022-12-05 23:08:03 +0100

    feat: :memo: add the two first categories routes on swagger
    - add get all categories | add get by id

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ const routes = require('./routes');
 const fileUpload = require('express-fileupload');
 const errorF = require('./utils/error');
 const cookieParser = require('cookie-parser');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const whitelist = [config.url_front];
 const corsOptions = {
   credentials: true, // This is important.
@@ -29,6 +32,37 @@ app.use(helmet());
 app.use(express.json());
 app.use(fileUpload());
 
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API for JSONPlaceholder',
+    version: '1.0.0',
+    description:
+      'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+    license: {
+      name: 'Licensed Under MIT',
+      url: 'https://spdx.org/licenses/MIT.html',
+    },
+    contact: {
+      name: 'JSONPlaceholder',
+      url: 'https://jsonplaceholder.typicode.com',
+    },
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000/api',
+      description: 'Development server',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./src/routes/*.js'],
+};
+const swaggerSpec = swaggerJSDoc(options);
+
 // Routes to test the API
 app.get('/', (req, res) => {
   res.json({
@@ -39,6 +73,8 @@ app.get('/', (req, res) => {
   });
 });
 
+
+
 // Handle syntax error
 app.use((error, req, res, next) => {
   if (error instanceof SyntaxError) {
@@ -48,8 +84,11 @@ app.use((error, req, res, next) => {
   }
 });
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 // All the api routes
 app.use('/api', routes);
+
 
 // Error handling not found
 app.use((req, res, next) => {
--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -8,10 +8,49 @@ const filterAllowed = ['name', 'description'];
 const router = express.Router();
 
 
-// ok 
-router.get('/', filterF(filterAllowed), categoryController.getAll); 
+/**
+ * @swagger
+ * /categories:
+ *   get:
+ *      produces:
+ *        - application/json
+ *      summary: Retrieve a list of all event categories
+ *      tags: [Categories]
+ *      description: Can be used to get a list of possible categories to search for events
+ *      responses:
+ *       200:
+ *         description: the list of the posts
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+*/
+router.get('/', filterF(filterAllowed), categoryController.getAll);
 
-// ok 
+/**
+* @swagger
+* /categories/{id}:
+*   get:
+*      produces:
+*        - application/json
+*      summary: Retrieve a category
+*      tags: [Categories]
+*      parameters:
+*        - in : path
+*          name: id
+*          description: id of category
+*          schema:
+*            type: string
+*          required: true
+*      description: Can be used to get a category by id
+*      responses:
+*       200:
+*         description: the list of the posts
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*/
 router.get('/:_id', validate(null), categoryController.getOne);
 
 // ok  
