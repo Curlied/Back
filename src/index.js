d93623b66bd22b41fbef0a440c817adaf7207bfd
Antonin BRINES <antonin.brines@outlook.fr>
2023-01-12 11:16:04 +0100

    fix: 🐛 ssl false

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const config = require('./config/index');
 const mongoose = require('mongoose');
 const port = config.port || 3000;
 
-let connection_string = `mongodb+srv://${config.database.username}:${config.database.password}@${config.database.url}/${config.database.name}?retryWrites=true&w=majority`
+let connection_string = `mongodb+srv://${config.database.username}:${config.database.password}@${config.database.url}/${config.database.name}?retryWrites=true&w=majority&ssl=false`;
 mongoose.connect(connection_string).then(() => {
   console.log('Connected to MongoDB');
   app.listen(config.port, () => {
