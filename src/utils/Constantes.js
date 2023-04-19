a2266398a5c11130b2eca6268df2e6f5c0a1955b
Antonin BRINES <antonin.brines@outlook.fr>
2023-04-19 10:55:31 +0200

    fix: 🐛 hot change url template

--- src/utils/Constantes.js
+++ src/utils/Constantes.js
@@ -53,7 +53,7 @@ module.exports = Object.freeze({
   },
   EMAIL_TEMPLATE: {
     PATH_CONFIRMATION_INSCRIPTION:
-      './src/templates/confirmation-inscription.html',
+      '/src/templates/confirmation-inscription.html',
   },
   STATUS_EVENT: {
     VALIDATE: 'Validé',
